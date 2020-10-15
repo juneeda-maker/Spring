522cea98259d40d28f6a74592446ff5fcd34f6b3
juneeda-maker <flora9511@daum.net>
2020-10-15 01:14:30 -0700

    화면처리

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -1,9 +1,10 @@
 package org.zerock.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.log4j.Log4j;
 
@@ -15,5 +16,16 @@ public class UploadController {
 	public void uploadForm() {
 		
 		log.info("upload form");
+	}
+	
+	@PostMapping("/uploadFormAction")
+	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
+		
+		for (MultipartFile multipartFile : uploadFile) {
+			
+			log.info("-------------");
+			log.info("Upload File Name: " +multipartFile.getOriginalFilename());
+			log.info("Upload File Size: " +multipartFile.getSize());
+		}
 	}
 }
