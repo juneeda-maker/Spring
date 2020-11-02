e9702af987a525a055a54f7b0ea899896161219f
juneeda-maker <flora9511@daum.net>
2020-11-01 22:41:24 -0800

    화면 조회 처리

--- ex_board/src/main/java/org/zerock/controller/BoardController.java
+++ ex_board/src/main/java/org/zerock/controller/BoardController.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
 
 
@@ -26,5 +30,51 @@ public class BoardController {
 		
 		model.addAttribute("list", service.getList());
 	}
-
+	
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		
+		log.info("register: " + board);
+		
+		service.register(board);
+		
+		rttr.addFlashAttribute("result", board.getBno());
+		
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		
+		log.info("/get");
+		model.addAttribute("board", service.get(bno)); //Model 객체를 사용하여 view 에 데이터 넘기기.
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify:" + board);
+		
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		
+		log.info("remove...." + bno);
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/register")
+	public void register() {
+		
+	}
+	
+	
 }
--- ex_board/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex_board/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -45,5 +45,52 @@ public class BoardControllerTests {
 				.getModelAndView()
 				.getModelMap());
 	}
+	
+	
+	@Test
+	public void testRegister()throws Exception{
+		
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+				.param("title", "테스트 새글 제목")
+				.param("content", "테스트 새글 내용")
+				.param("writer", "user00")
+				).andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+	}
+	
+	
+	@Test
+	public void testGet() throws Exception {
+		log.info(mockMvc.perform(MockMvcRequestBuilders
+				.get("/board/get")
+				.param("bno", "2"))
+				.andReturn()
+				.getModelAndView().getModelMap());
+	}
+	
+	@Test
+	public void testModify() throws Exception{
+		
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/modify")
+					.param("bno", "1")
+					.param("title", "수정된 테스트 새글 제목")
+					.param("content", "수정된 테스트 새글 내용")
+					.param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+	}
+	
+	@Test
+	public void testRemove()throws Exception {
+		//삭제전 데이터베이스에 게시물 번호 확인할것.
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove")
+				.param("bno", "23")
+				).andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+	}
 
 }
