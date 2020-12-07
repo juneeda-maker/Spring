a4254353a8936bdc5bb807d7b84e18fe9aae2ab9
juneeda-maker <flora9511@daum.net>
2020-12-08 00:45:07 +0900

    REST 방식을 이용한 Sample

--- ex_board/src/main/java/org/zerock/controller/SampleController.java
+++ ex_board/src/main/java/org/zerock/controller/SampleController.java
@@ -1,6 +1,6 @@
 package org.zerock.controller;
 
-import java.util.HashMap;
+
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -22,54 +22,23 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/sample")
 @Log4j
 public class SampleController {
-
-	@GetMapping(value ="/getText", produces = "text/plain; charset=UTF-8")
-	public String getText() {
+	
+	@GetMapping(value = "/getText", produces = "text/plain; charset=UTF-8")
+	public String getText(){
+		//value 속성에 매핑될 url, produces는 생산 가능한 미디어 타입의 목록을 지정해서 주요 매핑을 제한할수 있다
+		//produces에 명시한 media type과 같을때에 해당 타입으로 리스폰스를 보냄.
 		
 		log.info("MIME TYPE: " + MediaType.TEXT_PLAIN_VALUE);
 		
 		return "안녕하세요";
+		
 	}
 	
-	
 	@GetMapping(value = "/getSample",
-			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE,
-					MediaType.APPLICATION_XML_VALUE })
+			produces = { MediaType.APPLICATION_JSON_UTF8_VALUE,
+						MediaType.APPLICATION_XML_VALUE})
 	public SampleVO getSample() {
 		
 		return new SampleVO(112, "스타", "로드");
-	}
-	
-	@GetMapping(value = "/getList")
-	public List<SampleVO> getList() {
-		
-		return IntStream.range(1, 10).mapToObj(i -> new SampleVO(i, i + "First", i+"Last"))
-				.collect(Collectors.toList());
-	}
-	
-	@GetMapping(value = "/getMap")
-	public Map<String, SampleVO>getMap(){
-		
-		Map<String, SampleVO> map = new HashMap<>();
-		map.put("First", new SampleVO(111, "그루트", "주니어"));
-		
-		return map;
-	}
-	
-	@GetMapping(value = "/check", params = {"height", "weight"})
-	public ResponseEntity<SampleVO> check(Double height, Double weight){
-		
-		SampleVO vo = new SampleVO(0, "" + height, "" + weight);
-		
-		ResponseEntity<SampleVO> result = null;
-		
-		if(height < 150) {
-			result = ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(vo);
-			
-		}else {
-			result = ResponseEntity.status(HttpStatus.OK).body(vo);
-		}
-		
-		return result;
 	}
 }
