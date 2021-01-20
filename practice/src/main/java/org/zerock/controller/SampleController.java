75ef71015658a0a650b9abe5d3e9911718f66c16
juneeda-maker <flora9511@daum.net>
2021-01-20 23:58:21 +0900

    Controller example

--- practice/src/main/java/org/zerock/controller/SampleController.java
+++ practice/src/main/java/org/zerock/controller/SampleController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
@@ -24,13 +25,14 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SampleController {
 	
-	
+	/*
 	@InitBinder
 	public void initBinder(WebDataBinder binder)
 	{
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor (dateFormat, false));
 	}
+	*/
 	
 	
 	@RequestMapping("")
@@ -85,6 +87,15 @@ public class SampleController {
 	{
 		log.info("todo: " + todo);
 		return "ex03";
+	}
+	
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page")int page)
+	{
+		log.info("dto: " + dto);
+		log.info("page: " + page);
+		
+		return "/sample/ex04";
 	}
 	
 }
--- practice/src/main/java/org/zerock/domain/TodoDTO.java
+++ practice/src/main/java/org/zerock/domain/TodoDTO.java
@@ -2,11 +2,15 @@ package org.zerock.domain;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
 public class TodoDTO {
 	
 	private String title;
+	
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
 	private Date dueDate;
 }
